e1ceb8b5ed56e2110cad03476b61b760e9b6c958
ajanet379 <ajanet@unomaha.edu>
2018-02-17 21:19:10 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# EFS
+## A python-django project
+
+## Objectives
+1. Demonstrate basic HTML, Python skills in building a web application integrated with both a database and an external web services using an IDE (Integrated Development Environment).
+2. Demonstrate your ability to work with cloud based systems and on premise applications and services.
+3. Gain an understanding of the value of the MVC (Model View Controller)  and MV (Model View) Frameworks used in Python, Ruby, PHP, Java and  .NET today.
+4. Develop a web/database application.
+5. Demonstrate best practice code management by using Git on your laptop and Github as a code repository.  
+6. Demonstrate ability to build and execute functional test cases for an application.
+
---
+++ portfolio/forms.py
@@ -0,0 +1,29 @@
+
+from django import forms
+from .models import Customer, Investment, Stock , Fund
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ('cust_number', 'name', 'address', 'city', 'state', 'zipcode', 'email', 'cell_phone',)
+
+
+class InvestmentForm(forms.ModelForm):
+    class Meta:
+        model = Investment
+        fields = (
+            'customer', 'category', 'description', 'acquired_value', 'acquired_date', 'recent_value',
+            'recent_date',)
+
+
+class StockForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ('customer', 'symbol', 'name', 'shares', 'purchase_price', 'purchase_date',)
+
+
+class FundForm(forms.ModelForm):
+    class Meta:
+        model = Fund
+        fields = ('customer', 'fund_house', 'years', 'name', 'num_funds', 'launch_date',)
